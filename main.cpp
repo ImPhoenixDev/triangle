9b9c660d6c1e60da83de2feb869e36315d163bf8
Guillermo Loza <guillermo.loza@vantis.mx>
2022-11-17 20:12:10 -0600

    [feat] main.cpp: New language -> es.

--- main.cpp
+++ main.cpp
@@ -8,28 +8,28 @@ int main()
 {
 	triangulo t;
 
-	cout << "Enter the value of X in the point 1" << endl;
+	cout << "Valor en X del Punto 1" << endl;
 	double x1;
 	cin >> x1;
-	cout << "Enter the value of Y in the point 1" << endl;
+	cout << "Valor en Y del Punto 1" << endl;
 	double y1;
 	cin >> y1;
 	point p1(x1, y1);
 	t.agregarVertice(p1);
 	
-	cout << "Enter the value of X in the point 2" << endl;
+	cout << "Valor en X del Punto 2" << endl;
 	double x2;
 	cin >> x2;
-	cout << "Enter the value of Y in the point 2" << endl;
+	cout << "Valor en Y del Punto 2" << endl;
 	double y2;
 	cin >> y2;
 	point p2(x2, y2);
 	t.agregarVertice(p2);
 
-	cout << "Enter the value of X in the point 3" << endl;
+	cout << "Valor en X del Punto 3" << endl;
 	double x3;
 	cin >> x3;
-	cout << "Enter the value of Y in the point 3" << endl;
+	cout << "Valor en Y del Punto 3" << endl;
 	double y3;
 	cin >> y3;
 	point p3(x3, y3);
